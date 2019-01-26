a207d117301ef5eec965546a96f374456b9d80dd
Michael Snider <michaelsnider@knockrentals.com>
2019-01-26 14:44:25 -0800

    Increase test coverage of availability
    Update snapshots for bad responses

--- src/properties/availability.test.js
+++ src/properties/availability.test.js
@@ -2,11 +2,21 @@ import availability, { isValid } from './availability';
 
 describe('availability', () => {
   it('executes as expected', async () => {
-    expect(await availability({ pathParameters: { propertyId: '1234' } }, {})).toMatchSnapshot();
+    expect(
+      await availability({ pathParameters: { propertyId: '1234' } }, {})
+    ).toMatchSnapshot();
   });
 
-  it('is valid', async () => {
-    expect(await isValid({ propertyId: 'aaa' })).toEqual(false)
+  it('is invalid', async () => {
+    expect(
+      await availability({ pathParameters: { propertyId: 'aaaa' } }, {})
+    ).toMatchSnapshot();
     // TODO: ...
-  })
+  });
+  it('cannot be 123', async () => {
+    expect(
+      await availability({ pathParameters: { propertyId: '123' } }, {})
+    ).toMatchSnapshot();
+    // TODO: ...
+  });
 });
