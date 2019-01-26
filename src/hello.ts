1171103a0e9ffd8ad112581c9169feb3cb0db6a6
Michael Snider <michaelsnider@knockrentals.com>
2019-01-26 12:40:46 -0800

    Convert hello function to TypeScript

--- src/hello.ts
+++ src/hello.ts
@@ -1,23 +1,24 @@
 // TODO: remove as just generic example
 
-import fetch from 'node-fetch';
-import { successResponse, runWarm } from './utils';
-import foo from './services/foo';
+import fetch from "node-fetch";
+import foo from "./services/foo";
+import { runWarm, successResponse } from "./utils";
 
 const hello = async (/* event, context */) => {
   // // successResponse handles wrapping the response in an API Gateway friendly
   // // format (see other responses, including CORS, in `./utils/lambda-response.js)
   // const response = successResponse({
-  //   message: 'Yeah, baby! Go Serverless! Your function executed successfully!',
+  //   message: "Yeah, baby! Go Serverless! Your function executed successfully!",
   //   input: event,
   // });
   // callback(null, response);
 
-  const breweries = await fetch('https://api.openbrewerydb.org/breweries').then(res => res.json())
+  const breweries = await fetch("https://api.openbrewerydb.org/breweries").then(
+    (res) => res.json());
   const names = foo(breweries);
   return successResponse({ names });
 };
 
-// runWarm function handles pings from the scheduler so you don't
+// runWarm function handles pings from the scheduler so you don"t
 // have to put that boilerplate in your function.
 export default runWarm(hello);
